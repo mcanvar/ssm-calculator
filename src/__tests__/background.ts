169138f73ba7f1935b3cccc80f3ba56262b32fca
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-09-20 03:23:50 +0300

    implements some basic tests

--- src/__tests__/background.ts
+++ src/__tests__/background.ts
@@ -1,6 +1,23 @@
-import { logCurrentColor } from "../background";
+describe('Manifest', () => {
+    const manifestV2 = require('../../public/manifest-v2.json')
+    const manifestV3 = require('../../public/manifest-v3.json')
 
-test('Saved color should be valid 7 digit HEX color with dash.', () => {
-  // TODO: implement the test
-  expect(logCurrentColor()).toBe('#000000');
+    describe('v2 has', () => {
+        it('correct version property', () => {
+            expect(manifestV2).toHaveProperty('manifest_version', 2)
+        })
+    })
+
+    describe('v3 has', () => {
+        it('correct version property', () => {
+            expect(manifestV3).toHaveProperty('manifest_version', 3)
+        })
+    })
+})
+
+describe('background.js', () => {
+    it('Saved color should be valid 7 digit HEX color with dash.', () => {
+        // Refer testing for browser extension API:
+        // https://lusito.github.io/mockzilla-webextension/setup.html
+    })
 })
