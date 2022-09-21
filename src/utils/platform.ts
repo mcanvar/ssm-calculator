252c64f8b22bfe3df427760974f7357738c7d24b
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-09-21 04:44:24 +0300

    installs lint-staged

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,9 +1,9 @@
+import browser from 'webextension-polyfill'
+
 /*
 Some useful utils in case facing of browser related APIs such as Chrome setBadgeText API.
  */
 
-import browser from 'webextension-polyfill'
+export const isChrome: boolean = browser.runtime.getURL('').startsWith('chrome-extension://')
 
-export const isChrome = browser.runtime.getURL('').startsWith('chrome-extension://')
-
-export const isFirefox = browser.runtime.getURL('').startsWith('moz-extension://')
+export const isFirefox: boolean = browser.runtime.getURL('').startsWith('moz-extension://')
