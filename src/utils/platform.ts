2694da6bcbece305c20be331d969766d752d641b
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-09-21 04:41:06 +0300

    runs prettier

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -2,8 +2,8 @@
 Some useful utils in case facing of browser related APIs such as Chrome setBadgeText API.
  */
 
-import browser from "webextension-polyfill";
+import browser from 'webextension-polyfill'
 
-export const isChrome = browser.runtime.getURL('').startsWith('chrome-extension://');
+export const isChrome = browser.runtime.getURL('').startsWith('chrome-extension://')
 
 export const isFirefox = browser.runtime.getURL('').startsWith('moz-extension://')
